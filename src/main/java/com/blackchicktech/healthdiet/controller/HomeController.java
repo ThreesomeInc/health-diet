c842083923220779025d8d2968c575e0b3a230d4
Eric Cen <Eric_Cen@outlook.com>
2018-08-04 00:28:31 +0800

    Service

--- src/main/java/com/blackchicktech/healthdiet/controller/HomeController.java
+++ src/main/java/com/blackchicktech/healthdiet/controller/HomeController.java
@@ -1,9 +1,7 @@
 package com.blackchicktech.healthdiet.controller;
 
-import com.blackchicktech.healthdiet.domain.Nutrition;
 import com.blackchicktech.healthdiet.domain.ReportRequest;
 import com.blackchicktech.healthdiet.domain.ReportResponse;
-import com.blackchicktech.healthdiet.domain.SuggestNutrition;
 import com.blackchicktech.healthdiet.service.ReportService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -22,20 +20,6 @@ public class HomeController {
     @RequestMapping(value = "/report", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ResponseBody
     public ReportResponse getReport(ReportRequest reportRequest) {
-        ReportResponse response = new ReportResponse();
-        response.setHealthEstimation("Normal");
-        response.setAdvice("No Advice");
-        SuggestNutrition sn1 = new SuggestNutrition("energy", "1575", "kcal");
-        SuggestNutrition sn2 = new SuggestNutrition("protein", "45", "g");
-        response.addSuggestNutrition(sn1);
-        response.addSuggestNutrition(sn2);
-
-        Nutrition n1 = new Nutrition("milk", "220", "ml");
-        Nutrition n2 = new Nutrition("fruit", "176", "g");
-        Nutrition n3 = new Nutrition("egg", "1", "ge");
-        response.addNutrition(n1);
-        response.addNutrition(n2);
-        response.addNutrition(n3);
-        return response;
+        return reportService.report(reportRequest);
     }
 }
--- src/main/java/com/blackchicktech/healthdiet/service/ReportService.java
+++ src/main/java/com/blackchicktech/healthdiet/service/ReportService.java
@@ -1,7 +1,9 @@
 package com.blackchicktech.healthdiet.service;
 
+import com.blackchicktech.healthdiet.domain.Nutrition;
 import com.blackchicktech.healthdiet.domain.ReportRequest;
 import com.blackchicktech.healthdiet.domain.ReportResponse;
+import com.blackchicktech.healthdiet.domain.SuggestNutrition;
 import org.springframework.stereotype.Service;
 
 /**
@@ -11,6 +13,18 @@ import org.springframework.stereotype.Service;
 public class ReportService {
 
     public ReportResponse report(ReportRequest reportRequest){
-        return null;
+        ReportResponse response = new ReportResponse();
+        response.setHealthEstimation("Normal");
+        response.setAdvice("No Advice");
+        SuggestNutrition sn1 = new SuggestNutrition("energy", "1575", "kcal");
+        SuggestNutrition sn2 = new SuggestNutrition("protein", "45", "g");
+        response.addSuggestNutrition(sn1);
+        response.addSuggestNutrition(sn2);
+
+        Nutrition n1 = new Nutrition("milk", "220", "ml");
+        Nutrition n2 = new Nutrition("fruit", "176", "g");
+        Nutrition n3 = new Nutrition("egg", "1", "ge");
+        response.addNutrition(n1);
+        return response;
     }
 }
