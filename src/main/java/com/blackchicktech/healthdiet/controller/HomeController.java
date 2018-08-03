21d7005e5dafa4572eab933f08e0e1896b3503a3
Eric Cen <Eric_Cen@outlook.com>
2018-08-04 00:04:44 +0800

    service

--- src/main/java/com/blackchicktech/healthdiet/controller/HomeController.java
+++ src/main/java/com/blackchicktech/healthdiet/controller/HomeController.java
@@ -2,17 +2,24 @@ package com.blackchicktech.healthdiet.controller;
 
 import com.blackchicktech.healthdiet.domain.ReportRequest;
 import com.blackchicktech.healthdiet.domain.ReportResponse;
+import com.blackchicktech.healthdiet.service.ReportService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/home")
 public class HomeController {
 
+    @Autowired
+    public ReportService reportService;
 
     @RequestMapping(value = "/report", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ResponseBody
     public ReportResponse getReport(ReportRequest reportRequest) {
-        return new ReportResponse();
+        return reportService.report(reportRequest);
     }
 }
