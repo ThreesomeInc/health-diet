a28215e92880f5c264fd732e276a51cc2db68b3a
Eric Cen <Eric_Cen@outlook.com>
2018-08-26 17:55:51 +0800

    三日膳食分析 - 营养师建议

--- src/main/java/com/blackchicktech/healthdiet/service/FoodLogService.java
+++ src/main/java/com/blackchicktech/healthdiet/service/FoodLogService.java
@@ -8,6 +8,7 @@ import com.blackchicktech.healthdiet.entity.User;
 import com.blackchicktech.healthdiet.repository.FoodDaoImpl;
 import com.blackchicktech.healthdiet.repository.FoodLogDao;
 import com.blackchicktech.healthdiet.repository.UserDaoImpl;
+import com.blackchicktech.healthdiet.util.Constants;
 import com.blackchicktech.healthdiet.util.FoodLogUtil;
 import com.google.common.collect.ImmutableMap;
 import org.slf4j.Logger;
@@ -15,9 +16,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 @Service
@@ -187,6 +192,9 @@ public class FoodLogService {
         double na = 0.0d;
         double protein = 0.0;
         double energy = 0.0;
+        double ceRatio = 0.0;
+        double feRatio = 0.0;
+        double peRatio = 0.0;
         for(FoodLog foodLog : foodLogList){
             ca += foodLog.getCa();
             cho += foodLog.getCho();
@@ -196,17 +204,23 @@ public class FoodLogService {
             na += foodLog.getNa();
             protein += foodLog.getTotalProtein();
             energy += foodLog.getTotalEnergy();
+            ceRatio += foodLog.getCeRatio();
+            feRatio += foodLog.getFeRatio();
+            peRatio += foodLog.getPeRatio();
         }
 
         Map<String, Double> elementAvgs = ImmutableMap.<String, Double>builder()
                                             .put("钙", ca/3 )
                                             .put("碳水化合物", cho/3)
-                                            .put("脂肪", fat/3 )
+                                            .put("脂肪", fat/3)
                                             .put("钾", k/3)
                                             .put("磷", p/3)
                                             .put("钠", na/3)
                                             .put("蛋白质", protein/3)
                                             .put("能量", energy/3)
+                                            .put("碳水化合物供能比", ceRatio/3)
+                                            .put("脂肪供能比", feRatio/3)
+                                            .put("蛋白质供能比", peRatio/3)
                                             .build();
         return elementAvgs;
     }
@@ -228,12 +242,84 @@ public class FoodLogService {
         User user = userDao.getUserByOpenId(openId);
         double energy = elementAvgs.get("能量");
         double protein = elementAvgs.get("蛋白质");
+        double ceRatio = elementAvgs.get("碳水化合物供能比");
+        double feRatio = elementAvgs.get("脂肪供能比");
 
-        StringBuilder diticianAdvice = new StringBuilder();
+        StringBuilder diticianAdvice = new StringBuilder("");
 
         return diticianAdvice.toString();
 
 
+    }
+
+    private float calCalorie(User user) {
+        String sportRate = user.getSportRate();
+        float standardWeight = calStandardWeight(user);
+        float bmi = calBmiIndex(user);
+        if ("light".equals(sportRate)
+                || Constants.SPORT_RATE.getOrDefault("light", "").equals(sportRate)) {
+            if (bmi < 18.5) {
+                return standardWeight * 35;
+            } else if (bmi >= 18.5 && bmi < 23.9) {
+                return (float) (standardWeight * 32.5);
+            } else {
+                return standardWeight * 30;
+            }
+        } else {
+            if (bmi < 18.5) {
+                return standardWeight * 40;
+            } else if (bmi >= 18.5 && bmi < 23.9) {
+                return (standardWeight * 37);
+            } else {
+                return standardWeight * 35;
+            }
+        }
+
+
+    }
+
+    public float calStandardWeight(User user) {
+        String gender = user.getGender();
+        String height = getHeightOrWeight(user.getHeight());
+        if ("male".equals(gender) || Constants.GENDER.getOrDefault("male", "").equals(gender)) {
+            return (float) ((Integer.parseInt(height) - 100) * 0.9);
+        } else if ("female".equals(gender) || Constants.GENDER.getOrDefault("female", "").equals(gender)) {
+            return (float) ((Integer.parseInt(height) - 100) * 0.9 - 2.5);
+        } else {
+            throw new IllegalArgumentException("Gender can only be male or female");
+        }
+    }
+
+    private String getHeightOrWeight(String value) {
+        Matcher result = Pattern.compile("(\\d+(.\\d))?").matcher(value);
+        return result.find() ? result.group() : "0";
+    }
+
+    private float calBmiIndex(User user) {
+        String height = user.getHeight();
+        String weight = user.getWeight();
+        return (Float.parseFloat(getHeightOrWeight(weight)) / (Float.parseFloat(getHeightOrWeight(height)) / 100))
+                / (Float.parseFloat(getHeightOrWeight(height)) / 100);
+    }
+
+    private String calProtein(User user) {
+        int nephroticPeriod = Integer.parseInt(user.getNephroticPeriod());
+        List<String> treatmentMethod = Arrays.asList(user.getTreatmentMethod().split(","));
+        BigDecimal standardWeight = BigDecimal.valueOf(calStandardWeight(user));
+        StringBuilder protein = new StringBuilder();
+        if (nephroticPeriod >= 1 && nephroticPeriod <= 2) {
+            return protein.append(standardWeight.multiply(BigDecimal.valueOf(0.8)).setScale(1, BigDecimal.ROUND_FLOOR)).append("~")
+                    .append(standardWeight.multiply(BigDecimal.ONE).setScale(1, BigDecimal.ROUND_FLOOR)).append("克").toString();
+        } else if (nephroticPeriod >= 3 && nephroticPeriod <= 5) {
+            if (treatmentMethod.stream().anyMatch(item -> item.contains("dialysis"))) {
+                return protein.append(standardWeight.multiply(BigDecimal.ONE).setScale(1, BigDecimal.ROUND_FLOOR)).append("~")
+                        .append(standardWeight.multiply(BigDecimal.valueOf(1.2)).setScale(1, BigDecimal.ROUND_FLOOR)).append("克").toString();
+            } else {
+                return protein.append(standardWeight.multiply(BigDecimal.valueOf(0.6)).setScale(1, BigDecimal.ROUND_FLOOR)).append("~")
+                        .append(standardWeight.multiply(BigDecimal.valueOf(10.8)).setScale(1, BigDecimal.ROUND_FLOOR)).append("克").toString();
+            }
+        }
+        return "Protein is unclear.";
     }
 
 }
