55e52ba06060893117730c183a39bb07da28f47c
Eric Cen <Eric_Cen@outlook.com>
2018-08-26 23:30:39 +0800

    三日膳食分析 - 修复Double类型，添加Unit Test

--- src/main/java/com/blackchicktech/healthdiet/service/FoodLogService.java
+++ src/main/java/com/blackchicktech/healthdiet/service/FoodLogService.java
@@ -213,17 +213,17 @@ public class FoodLogService {
         }
 
         Map<String, Double> elementAvgs = ImmutableMap.<String, Double>builder()
-                                            .put("钙", ca/3 )
-                                            .put("碳水化合物", cho/3)
-                                            .put("脂肪", fat/3)
-                                            .put("钾", k/3)
-                                            .put("磷", p/3)
-                                            .put("钠", na/3)
-                                            .put("蛋白质", protein/3)
-                                            .put("能量", energy/3)
-                                            .put("碳水化合物供能比", ceRatio/3)
-                                            .put("脂肪供能比", feRatio/3)
-                                            .put("蛋白质供能比", peRatio/3)
+                                            .put("钙", BigDecimal.valueOf(ca/3).setScale(1, BigDecimal.ROUND_FLOOR).doubleValue() )
+                                            .put("碳水化合物", BigDecimal.valueOf(cho/3).setScale(1, BigDecimal.ROUND_FLOOR).doubleValue())
+                                            .put("脂肪", BigDecimal.valueOf(fat/3).setScale(1, BigDecimal.ROUND_FLOOR).doubleValue())
+                                            .put("钾", BigDecimal.valueOf(k/3).setScale(1, BigDecimal.ROUND_FLOOR).doubleValue())
+                                            .put("磷", BigDecimal.valueOf(p/3).setScale(1, BigDecimal.ROUND_FLOOR).doubleValue())
+                                            .put("钠", BigDecimal.valueOf(na/3).setScale(1, BigDecimal.ROUND_FLOOR).doubleValue())
+                                            .put("蛋白质", BigDecimal.valueOf(protein/3).setScale(1, BigDecimal.ROUND_FLOOR).doubleValue())
+                                            .put("能量", BigDecimal.valueOf(energy/3).setScale(1, BigDecimal.ROUND_FLOOR).doubleValue())
+                                            .put("碳水化合物供能比", BigDecimal.valueOf(ceRatio/3).setScale(1, BigDecimal.ROUND_FLOOR).doubleValue())
+                                            .put("脂肪供能比", BigDecimal.valueOf(feRatio/3).setScale(1, BigDecimal.ROUND_FLOOR).doubleValue())
+                                            .put("蛋白质供能比", BigDecimal.valueOf(peRatio/3).setScale(1, BigDecimal.ROUND_FLOOR).doubleValue())
                                             .build();
         return elementAvgs;
     }
---
+++ src/test/java/com/blackchicktech/healthdiet/service/FoodLogServiceTest.java
@@ -0,0 +1,74 @@
+package com.blackchicktech.healthdiet.service;
+
+import com.blackchicktech.healthdiet.entity.FoodLog;
+import com.blackchicktech.healthdiet.repository.FoodDaoImpl;
+import com.blackchicktech.healthdiet.repository.FoodLogDao;
+import com.blackchicktech.healthdiet.repository.UserDaoImpl;
+import com.google.common.collect.ImmutableList;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by Eric Cen on 2018/8/26.
+ */
+public class FoodLogServiceTest {
+
+    @Mock
+    private FoodLogService foodLogService;
+
+    @Mock
+    private FoodDaoImpl foodDao;
+
+    @Mock
+    private UserDaoImpl userDao;
+
+    @Mock
+    private FoodLogDao foodLogDao;
+
+    private List<FoodLog> foodLogList = ImmutableList.<FoodLog>builder()
+                                        .add(new FoodLog("test",new Date(2018,8, 28),
+                                                    true, 2500, 3000,0.6, 30.5, 0.5, 56.2, 0.5, 4.5,
+                                                               5.5, 3.6, 4.6))
+                                        .add(new FoodLog("test",new Date(2018,8, 25),
+                                                true, 2500, 3000,0.6, 30.5, 0.5, 56.2, 0.5, 4.5,
+                                                5.5, 3.6, 4.6))
+                                        .add(new FoodLog("test",new Date(2018,8, 24),
+                                                true, 2500, 3000,0.6, 30.5, 0.5, 56.2, 0.5, 4.5,
+                                                5.5, 3.6, 4.6)).build();
+
+
+    @Before
+    public void setUp(){
+        foodLogService = Mockito.mock(FoodLogService.class);
+        foodDao = Mockito.mock(FoodDaoImpl.class);
+        userDao = Mockito.mock(UserDaoImpl.class);
+        foodLogDao = Mockito.mock(FoodLogDao.class);
+    }
+
+    @Test
+    public void testDeduceElementEvgs(){
+        when(foodLogService.deduceElementEvgs(foodLogList)).thenCallRealMethod();
+        Map<String, Double> result = foodLogService.deduceElementEvgs(foodLogList);
+        Assert.assertEquals(result.get("钙").doubleValue(), 4.6, 0.0);
+        Assert.assertEquals(result.get("碳水化合物").doubleValue(), 56.2, 0.0);
+        Assert.assertEquals(result.get("碳水化合物供能比").doubleValue(), 0.5, 0.0);
+    }
+
+    @Test
+    public void testIsStandardLogType(){
+        when(foodLogService.isStandardLogType(foodLogList)).thenCallRealMethod();
+        boolean isStandardLogType = foodLogService.isStandardLogType(foodLogList);
+        Assert.assertFalse(isStandardLogType);
+    }
+
+
+}
