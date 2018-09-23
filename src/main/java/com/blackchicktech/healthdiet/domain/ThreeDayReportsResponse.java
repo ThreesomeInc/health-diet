ad67426c8c5bce8c29bb46501c67fcc9f9e36d98
martinho <martinho0330@gmail.com>
2018-09-23 08:06:16 +0800

    change to date only

--- src/main/java/com/blackchicktech/healthdiet/domain/ThreeDayReportsResponse.java
+++ src/main/java/com/blackchicktech/healthdiet/domain/ThreeDayReportsResponse.java
@@ -3,17 +3,21 @@ package com.blackchicktech.healthdiet.domain;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.time.temporal.ChronoField;
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ThreeDayReportsResponse extends BasicResponse {
 
     @JsonProperty("logDateList")
-    private List<Date> logDateList;
+    private List<Integer> logDateList;
 
     public ThreeDayReportsResponse(List<Date> logDateList) {
-        this.logDateList = logDateList;
+        this.logDateList = logDateList.stream().filter(Objects::nonNull)
+                .map(item -> item.toInstant().get(ChronoField.DAY_OF_MONTH)).collect(Collectors.toList());
     }
 
     @Override
