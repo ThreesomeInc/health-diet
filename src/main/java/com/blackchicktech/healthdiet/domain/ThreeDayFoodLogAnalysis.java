53c36ee6c2e5b9f1d8ba941a6b73fbfb752b96c0
SaraQian <abigail830@163.com>
2018-10-07 12:25:16 +0800

    Adding swappger annotation for request/response object

--- src/main/java/com/blackchicktech/healthdiet/domain/ThreeDayFoodLogAnalysis.java
+++ src/main/java/com/blackchicktech/healthdiet/domain/ThreeDayFoodLogAnalysis.java
@@ -2,6 +2,7 @@ package com.blackchicktech.healthdiet.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
 
 import java.util.List;
 import java.util.Map;
@@ -13,6 +14,7 @@ import java.util.Map;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ThreeDayFoodLogAnalysis {
 
+    @ApiModelProperty(value = "标准三日膳食标注", example = "true")
     @JsonProperty
     private Boolean standardLog;
 
